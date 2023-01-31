383e084cb55b05ce153c927645bf0db028f56232
Brian Pelaez <pelaez_brian@hotmail.com>
2023-01-31 11:15:32 -0300

    First Endpoint

--- index.js
+++ index.js
@@ -2,31 +2,40 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+require('dotenv').config()
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+app.use('/', (req, res, next) => {
+  console.log(req.method + " "+req.path)
+  next();
+})
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+// your first API endpoint...
+app.get("/api/2015-12-25", function (req, res) {
+  req.time = new Date(2015,11,25);
+  res.json({
+    unix: req.time.getTime(),
+    utc: req.time.toUTCString(),
+  });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
